880184ccbd884ed2b18d5d3a929fb9b815d5c0d9
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-07-21 19:29:28 -0400

    updated: nav bar headers

--- matthew-tzou-website/src/NavBar.svelte
+++ matthew-tzou-website/src/NavBar.svelte
@@ -6,18 +6,18 @@
     </div>
     <div>
         <ul class="flex align-middle">
-            <li class="ml-6 align-middle no-underline">
-              <a class="text-white hover:no-underline visited:text-white" href="/">About Me</a>
-            </li>
-            <li class="ml-6">
+            <li class="ml-12">
               <a class="text-white hover:no-underline visited:text-green-700" href="/">Experience</a>
             </li>
-            <li class="ml-6">
-              <a class="text-white hover:no-underline visited:text-white" href="/">Contact Me</a>
+            <li class="ml-12">
+              <a class="text-white hover:no-underline visited:text-white" href="/">Projects</a>
             </li>
-            <li class="ml-6">
-              <a class="text-gray-400 cursor-not-allowed visited:text-white" href="/">Disabled</a>
+            <li class="ml-12">
+              <a class="text-gray-400 cursor-not-allowed visited:text-white" href="/">Links</a>
             </li>
+            <li class="ml-12">
+                <a class="text-gray-400 cursor-not-allowed visited:text-white" href="/">Contact Me</a>
+              </li>
           </ul>
     </div>
 
