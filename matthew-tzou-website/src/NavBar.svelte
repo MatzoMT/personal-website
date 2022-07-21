beb9e1a162adfbc2318a2ca1b603f70102f2af8d
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-07-21 19:22:37 -0400

    fixed: nav bar visited colors

--- matthew-tzou-website/src/NavBar.svelte
+++ matthew-tzou-website/src/NavBar.svelte
@@ -7,26 +7,31 @@
     <div>
         <ul class="flex align-middle">
             <li class="ml-6 align-middle no-underline">
-              <a class="text-white hover:no-underline visited:text-white">About Me</a>
+              <a class="text-white hover:no-underline visited:text-white" href="/">About Me</a>
             </li>
             <li class="ml-6">
-              <a class="text-white hover:no-underline visited:text-white">Experience</a>
+              <a class="text-white hover:no-underline visited:text-green-700" href="/">Experience</a>
             </li>
             <li class="ml-6">
-              <a class="text-white hover:no-underline visited:text-white">Contact Me</a>
+              <a class="text-white hover:no-underline visited:text-white" href="/">Contact Me</a>
             </li>
             <li class="ml-6">
-              <a class="text-gray-400 cursor-not-allowed visited:text-white">Disabled</a>
+              <a class="text-gray-400 cursor-not-allowed visited:text-white" href="/">Disabled</a>
             </li>
           </ul>
     </div>
 
 </nav>
 
+
+
 <style>
   nav {
     position: fixed;
     width: 100%;
     height: 4em;
+  }
+  a:visited {
+    color: white;
   }
 </style>
