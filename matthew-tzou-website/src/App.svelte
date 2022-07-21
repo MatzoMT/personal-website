3634635122abcc1217d6d1c8611bdac4411e7205
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-07-21 18:42:33 -0400

    added: NavBar.svelte

--- matthew-tzou-website/src/App.svelte
+++ matthew-tzou-website/src/App.svelte
@@ -1,17 +1,26 @@
 <script>
 	export let name;
 	import Experience from './Experience.svelte';
+	import NavBar from './NavBar.svelte';
 	let src="./meiniceland.png";
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
+</svelte:head>
 
+	<NavBar /> 
 <section class="w-full h-screen">
 	<img
 	  src="images/meiniceland.jpeg"
 	  class="object-cover w-full h-full"
 	  alt="Image alt text"
 	/> 
-  </section>
+
+</section>
+<h1>Playing with svelte</h1>
 
 <style global lang="postcss">
 	@tailwind base;
@@ -24,5 +33,6 @@ body {
     margin: 0;
     padding: 0;
     overflow-x: hidden; 
+	font-family: 'Questrial';
 }
   </style>
---
+++ matthew-tzou-website/src/NavBar.svelte
@@ -0,0 +1,27 @@
+
+
+<nav class="flex items-center p-6">
+    <ul class="flex align-middle">
+    <li class="mr-6 align-middle">
+      <a class="text-blue-500 hover:text-blue-800" href="#">About Me</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Experience</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Contact Me</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+    </li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    background-color: #000000;
+    position: fixed;
+    width: 100%;
+    height: 4em;
+  }
+</style>
